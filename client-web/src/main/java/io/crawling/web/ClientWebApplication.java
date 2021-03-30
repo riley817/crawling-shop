624430296c9075b4f51547ba00f1c916b53caecb
yj.yoon <yj.yoon@autowini.com>
2021-03-30 13:34:00 +0900

    20210330

---
+++ client-web/src/main/java/io/crawling/web/ClientWebApplication.java
@@ -0,0 +1,13 @@
+package io.crawling.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientWebApplication.class, args);
+    }
+
+}
---
+++ client-web/src/test/java/io/crawling/web/ClientWebApplicationTests.java
@@ -0,0 +1,13 @@
+package io.crawling.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
